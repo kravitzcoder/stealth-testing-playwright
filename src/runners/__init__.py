d0e9688d3dcb83fa6bccc3c2018606d8423fdf75
kravitzcoder <kravitzsidney@gmail.com>
2025-10-10 12:53:54 +0300

    Update __init__.py

--- src/runners/__init__.py
+++ src/runners/__init__.py
@@ -1,4 +1,19 @@
-"""Playwright category runner"""
-from .playwright_runner import PlaywrightRunner
+"""
+Specialized runners for each browser library
 
-__all__ = ['PlaywrightRunner']
+Each library has its own optimized runner!
+"""
+
+from . import base_runner
+from . import playwright_runner
+from . import patchright_runner
+from . import camoufox_runner
+from . import rebrowser_runner
+
+__all__ = [
+    'base_runner',
+    'playwright_runner',
+    'patchright_runner',
+    'camoufox_runner',
+    'rebrowser_runner'
+]
